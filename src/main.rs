c2b063f3167b85e867cbeb8cf69b12c7bb0eadea
elprebit <junichinomori@yahoo.co.jp>
2024-09-26 20:06:50 +0900

    add new feature to  gen_rand_points_in_area & fix player movement collision

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,10 @@ use crossterm::{
     terminal::{Clear, ClearType, EnterAlternateScreen, LeaveAlternateScreen},
 };
 use rand::{rngs::ThreadRng, seq::SliceRandom, Rng};
-use std::io::{self, Read, Write};
+use std::{
+    io::{self, Read, Write},
+    ops::Mul,
+};
 
 struct Coordinate {
     x: usize,
@@ -66,26 +69,42 @@ fn gen_rand_points_in_area(
     from_y: usize,
     width: usize,
     height: usize,
-    min_points_num: Option<usize>,
-    max_points_num: Option<usize>,
+    how_many_generate: Option<usize>,
+    max_num_of_rand_gen_points: Option<usize>,
+    exclusive_points: Vec<Coordinate>,
 ) -> Vec<Coordinate> {
     //! from_x <= x <= from_x + height、from_y <= y <= from_y + height
     //! from_x <= x <= from_x + height、from_y <= y <= from_y + height
     //! の範囲で 重複なく必要座標数分ランダムな座標のVecを生成する。
     //! 実装：領域の全ての座標を一つのVecに格納してシャッフル、必要な座標の数だけ取り出す。
-    //! TODO: use max_points_num
+    //! width * height - how_many_generate < exclusive_points.len()ならpanic!
+    //! exclusive_pointsの実装
+    //! pointsからexclusive_pointsの座標を除外すればよい
     let mut rng = rand::thread_rng();
-    let points_num = if let Some(min_points_num) = min_points_num {
-        if min_points_num > width * height {
+    let points_num = if let Some(how_many_generate) = how_many_generate {
+        if width * height - how_many_generate < exclusive_points.len() {
+            panic!("num of points to be excluded is too large to satisfy num of points to be generated")
+        };
+        if how_many_generate > width * height {
             panic!("points overflows area size")
         };
-        min_points_num
+        how_many_generate
     } else {
-        rng.gen_range(0..width * height)
+        if let Some(max_num) = max_num_of_rand_gen_points {
+            rng.gen_range(0..max_num)
+        } else {
+            rng.gen_range(0..width * height)
+        }
     };
     let mut points: Vec<Coordinate> = Vec::new();
     for y in from_y..from_y + height {
         for x in from_x..from_x + width {
+            if exclusive_points
+                .iter()
+                .any(|coord| coord.x == x && coord.y == y)
+            {
+                continue;
+            }
             points.push(Coordinate { x: x, y: y });
         }
     }
@@ -99,16 +118,22 @@ mod tests {
     use super::*;
     #[test]
     fn test_random_points_in_area_valid_points_num() {
-        assert_eq!(6, gen_rand_points_in_area(0, 0, 6, 6, Some(6), None).len());
+        assert_eq!(
+            6,
+            gen_rand_points_in_area(0, 0, 6, 6, Some(6), None, vec![]).len(),
+        );
     }
     #[test]
     fn test_random_points_in_points_fullfil_area() {
-        assert_eq!(9, gen_rand_points_in_area(0, 0, 3, 3, Some(9), None).len());
+        assert_eq!(
+            9,
+            gen_rand_points_in_area(0, 0, 3, 3, Some(9), None, vec![]).len()
+        );
     }
     #[test]
     #[should_panic]
     fn test_random_points_in_area_points_num_overflows_area_size() {
-        gen_rand_points_in_area(0, 0, 2, 2, Some(6), None).len();
+        gen_rand_points_in_area(0, 0, 2, 2, Some(6), None, vec![]).len();
     }
 }
 
@@ -117,37 +142,52 @@ fn main() {
     println!("Ascension Army");
     println!("Created by elprebit");
     pause_for_input();
-    let dungeon_floor = DungeonFloor {
+    let mut dungeon_floor = DungeonFloor {
         height: 16,
         width: 16,
         fog_of_wars: vec![],
         mobs: vec![],
     };
-    let mut mob_list: Vec<Mob> = Vec::new();
     let mut rng = rand::thread_rng();
-    let enemy_coordinates =
-        gen_rand_points_in_area(0, 0, dungeon_floor.width, dungeon_floor.height, None, None);
+    let initial_player_pos = (
+        rng.gen_range(0..dungeon_floor.width),
+        rng.gen_range(0..dungeon_floor.height),
+    );
+    dungeon_floor.mobs.push(Mob {
+        tag: String::from("player"),
+        pos: Coordinate {
+            x: initial_player_pos.0,
+            y: initial_player_pos.1,
+        },
+        strength: 2,
+        hp: 3,
+    });
+    let enemy_coordinates = gen_rand_points_in_area(
+        0,
+        0,
+        dungeon_floor.width,
+        dungeon_floor.height,
+        None,
+        Some(dungeon_floor.width * dungeon_floor.height - 1),
+        vec![Coordinate {
+            x: initial_player_pos.0,
+            y: initial_player_pos.1,
+        }],
+    );
     for coordinates in enemy_coordinates {
-        mob_list.push(Mob {
+        dungeon_floor.mobs.push(Mob {
             tag: String::from("enemy"),
             pos: coordinates,
             strength: 1,
             hp: 1,
         });
     }
-    let mut player = Mob {
-        tag: String::from("Player"),
-        pos: Coordinate {
-            x: rng.gen_range(0..dungeon_floor.width),
-            y: rng.gen_range(0..dungeon_floor.height),
-        },
-        strength: 2,
-        hp: 3,
-    };
+
     let mut player_movement = Vec2d { x: 0, y: 0 };
     execute!(stdout, Hide, EnterAlternateScreen).unwrap();
     'game: loop {
         let event = read().unwrap();
+        let enemy_map = dungeon_floor.mob_maskmap();
         match event {
             Event::Key(event) => {
                 if event.kind == KeyEventKind::Press {
@@ -181,21 +221,47 @@ fn main() {
                             player_movement.x = 1;
                             player_movement.y = 1;
                         }
+                        KeyCode::Left => {
+                            player_movement.x = -1;
+                        }
+                        KeyCode::Right => {
+                            player_movement.x = 1;
+                        }
+                        KeyCode::Up => {
+                            player_movement.y = -1;
+                        }
+                        KeyCode::Down => {
+                            player_movement.y = 1;
+                        }
                         _ => (),
                     }
                 }
-                if let Some(new_x) = player.pos.x.checked_add_signed(player_movement.x) {
-                    player.pos.x = if new_x >= dungeon_floor.width {
-                        dungeon_floor.width - 1
-                    } else {
-                        new_x
-                    }
-                }
-                if let Some(new_y) = player.pos.y.checked_add_signed(player_movement.y) {
-                    player.pos.y = if new_y >= dungeon_floor.height {
-                        dungeon_floor.height - 1
-                    } else {
-                        new_y
+                for mob in &mut dungeon_floor.mobs {
+                    if mob.tag == "player" {
+                        let new_x =
+                            if let Some(new_x) = mob.pos.x.checked_add_signed(player_movement.x) {
+                                if new_x > dungeon_floor.width - 1 {
+                                    dungeon_floor.width - 1
+                                } else {
+                                    new_x
+                                }
+                            } else {
+                                mob.pos.x
+                            };
+                        let new_y =
+                            if let Some(new_y) = mob.pos.y.checked_add_signed(player_movement.y) {
+                                if new_y > dungeon_floor.height - 1 {
+                                    dungeon_floor.height - 1
+                                } else {
+                                    new_y
+                                }
+                            } else {
+                                mob.pos.y
+                            };
+                        if !enemy_map[new_y][new_x] {
+                            mob.pos.y = new_y;
+                            mob.pos.x = new_x;
+                        }
                     }
                 }
             }
@@ -204,16 +270,19 @@ fn main() {
         player_movement.x = 0;
         player_movement.y = 0;
         let mut map_display = vec![vec!['.'; dungeon_floor.width]; dungeon_floor.height];
-        for mob in &mut mob_list {
+        for mob in &dungeon_floor.mobs {
             match mob.tag.as_str() {
                 "enemy" => {
                     map_display[mob.pos.y][mob.pos.x] = 'E';
                 }
+                "player" => {
+                    map_display[mob.pos.y][mob.pos.x] = '@';
+                }
                 _ => (),
             }
         }
-        map_display[player.pos.y][player.pos.x] = '@';
         queue!(stdout, Clear(ClearType::All)).unwrap();
+        queue!(stdout, MoveTo(0, 0), Print("left: h, down: j, up: k, right: l, leftup: u, leftdown: b, rightup: y, rightdown: n")).unwrap();
         for (y, row) in map_display.iter().enumerate() {
             for (x, cell) in row.iter().enumerate() {
                 queue!(stdout, MoveTo(x as u16, y as u16 + 1), Print(cell)).unwrap();
