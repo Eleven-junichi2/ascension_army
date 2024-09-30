2c9fb117922de5dbe5a63ddd0ee3167452caf508
elprebit <junichinomori@yahoo.co.jp>
2024-09-30 23:55:35 +0900

    refactor code

--- src/game.rs
+++ src/game.rs
@@ -1,4 +1,4 @@
-use rand::Rng;
+use rand::{seq::SliceRandom, Rng};
 
 #[derive(Debug, PartialEq)]
 pub struct Coordinate {
@@ -69,5 +69,80 @@ impl DungeonFloor {
             }
         }
         None
+    }
+}
+
+pub fn gen_rand_points_in_area(
+    from_x: usize,
+    from_y: usize,
+    width: usize,
+    height: usize,
+    how_many_generate: Option<usize>,
+    max_num_of_rand_gen_points: Option<usize>,
+    exclusive_points: Vec<Coordinate>,
+) -> Vec<Coordinate> {
+    //! from_x <= x <= from_x + height、from_y <= y <= from_y + height
+    //! from_x <= x <= from_x + height、from_y <= y <= from_y + height
+    //! の範囲で 重複なく必要座標数分ランダムな座標のVecを生成する。
+    //! 実装：領域の全ての座標を一つのVecに格納してシャッフル、必要な座標の数だけ取り出す。
+    //! width * height - how_many_generate < exclusive_points.len()ならpanic!
+    //! exclusive_pointsの実装
+    //! pointsからexclusive_pointsの座標を除外すればよい
+    //! # Panics
+    //! If `width * height - how_many_generate < exclusive_points.len()` or `how_many_generate > width * height`.
+    let mut rng = rand::thread_rng();
+    let points_num = if let Some(how_many_generate) = how_many_generate {
+        if width * height - how_many_generate < exclusive_points.len() {
+            panic!("num of points to be excluded is too large to satisfy num of points to be generated")
+        };
+        if how_many_generate > width * height {
+            panic!("points overflows area size")
+        };
+        how_many_generate
+    } else {
+        if let Some(max_num) = max_num_of_rand_gen_points {
+            rng.gen_range(0..max_num)
+        } else {
+            rng.gen_range(0..width * height)
+        }
+    };
+    let mut points: Vec<Coordinate> = Vec::new();
+    for y in from_y..from_y + height {
+        for x in from_x..from_x + width {
+            if exclusive_points
+                .iter()
+                .any(|coord| coord.x == x && coord.y == y)
+            {
+                continue;
+            }
+            points.push(Coordinate { x: x, y: y });
+        }
+    }
+    points.shuffle(&mut rng);
+    points.truncate(points_num);
+    points
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_random_points_in_area_valid_points_num() {
+        assert_eq!(
+            6,
+            gen_rand_points_in_area(0, 0, 6, 6, Some(6), None, vec![]).len(),
+        );
+    }
+    #[test]
+    fn test_random_points_in_points_fullfil_area() {
+        assert_eq!(
+            9,
+            gen_rand_points_in_area(0, 0, 3, 3, Some(9), None, vec![]).len()
+        );
+    }
+    #[test]
+    #[should_panic]
+    fn test_random_points_in_area_points_num_overflows_area_size() {
+        gen_rand_points_in_area(0, 0, 2, 2, Some(6), None, vec![]).len();
     }
 }
